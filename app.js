539d1365f5923f08d9330a3336b12900083f8bff
Abenetwolde <abnetwoldedev@gmail.com>
2024-10-17 17:12:02 +0300

    please work!

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const errorController = require("./controllers/errorController");
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors());
+
 app.use(cors({
   origin: "https://messenger-seven-delta.vercel.app", // Replace with your frontend domain
   credentials: true,  // Allow cookies to be sent with requests
