5565c4e52fdc2aa1a7584ab35cad7c8206ed6cc8
Abenetwolde <abnetwoldedev@gmail.com>
2024-10-18 10:04:45 +0300

    api working test

--- server.js
+++ server.js
@@ -15,7 +15,14 @@ mongoose
  
 // Serve client folder
 app.use(express.static(path.join(__dirname, "client", "build")));
-
+app.get("/api/test",async (req, res, next) => {
+  try {
+ res.send("the api is working....") 
+  } catch (error) {
+    console.log(error)
+  }
+  
+});
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
